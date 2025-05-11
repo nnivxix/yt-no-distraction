db9bd777f5013311d4a15ffba092d6573a5bda75
nnivxix <adhe.hanasa@gmail.com>
2025-05-11 16:40:48 +0700

    init

---
+++ wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-vue"],
+  manifest: {
+    name: "YT No Distraction",
+    description: "A YouTube extension that removes distractions.",
+    version: "1.0.0",
+  },
+});
