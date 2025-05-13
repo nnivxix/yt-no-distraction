7c47246fddeae01f061bae8577879402bc9e2484
nnivxix <adhe.hanasa@gmail.com>
2025-05-13 15:49:41 +0700

    feat: implement switch functionality to toggle YouTube sidebar visibility

---
+++ composables/useSwitch.ts
@@ -0,0 +1,50 @@
+import { ref } from "vue";
+import { browser } from "wxt/browser";
+
+const useSwitch = () => {
+  const switchValue = ref(false);
+  const toggleSwitch = async () => {
+    const tabs = await browser.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+
+    if (tabs.length > 0) {
+      const currentTab = tabs[0];
+
+      if (!currentTab.url?.includes("youtube.com/watch")) {
+        console.error("Not a YouTube watch page.");
+        switchValue.value = false;
+        return;
+      }
+
+      if (currentTab.id) {
+        await browser.scripting.executeScript({
+          target: { tabId: currentTab.id },
+          func: (isCheckedValue: boolean) => {
+            // This function runs in the context of the page
+            const secondaryId = document.getElementById("secondary");
+
+            if (!secondaryId) {
+              console.error("Element with ID 'secondary' not found.");
+              return;
+            }
+
+            if (isCheckedValue) {
+              secondaryId.style.display = "none";
+            } else {
+              secondaryId.style.display = "block";
+            }
+          },
+          args: [switchValue.value],
+        });
+      }
+    }
+  };
+  return {
+    switchValue,
+    toggleSwitch,
+  };
+};
+
+export default useSwitch;
--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,55 +1,15 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { browser } from "wxt/browser";
+import useSwitch from "@/composables/useSwitch";
 import Switch from "@/components/Switch.vue";
 
-const isChecked = ref(false);
-
-const handleClick = async () => {
-  const tabs = await browser.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-
-  if (tabs.length > 0) {
-    const currentTab = tabs[0];
-
-    if (!currentTab.url?.includes("youtube.com/watch")) {
-      console.error("Not a YouTube watch page.");
-      isChecked.value = false;
-      return;
-    }
-
-    if (currentTab.id) {
-      await browser.scripting.executeScript({
-        target: { tabId: currentTab.id },
-        func: (isCheckedValue: boolean) => {
-          // This function runs in the context of the page
-          const secondaryId = document.getElementById("secondary");
-
-          if (!secondaryId) {
-            console.error("Element with ID 'secondary' not found.");
-            return;
-          }
-
-          if (isCheckedValue) {
-            secondaryId.style.display = "none";
-          } else {
-            secondaryId.style.display = "block";
-          }
-        },
-        args: [isChecked.value],
-      });
-    }
-  }
-};
+const { switchValue, toggleSwitch } = useSwitch();
 </script>
 
 <template>
   <div>
     <h1>Toggle YouTube Sidebar</h1>
     <div class="switch-container">
-      <Switch v-model="isChecked" @click="handleClick" id="switch" />
+      <Switch v-model="switchValue" @click="toggleSwitch" id="switch" />
       <label for="switch">Hide Sidebar</label>
     </div>
   </div>
